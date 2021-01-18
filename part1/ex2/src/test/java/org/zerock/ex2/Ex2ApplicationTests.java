395bbe19a0c2467a698b67e793537c3aa32d2bee
godo <godo@AL01590066.local>
2021-01-18 18:43:39 +0900

    add Chapter1 to Chapter2 of Part1

---
+++ part1/ex2/src/test/java/org/zerock/ex2/Ex2ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.ex2.repository.MemoRepositoryTests;
+
+@SpringBootTest
+class Ex2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
